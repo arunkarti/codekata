8afcdbf95a893554fff2345e1bede44c37210ecf
ak <arunkarti@gmail.com>
2017-09-04 22:24:30 +0530

    Update pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -5,43 +5,34 @@ int main()
 	int n,count=0,k=1;
 	cin>>n;
 	int u=n;
-	for(int i=1;i<n;i++)
+	while(u!=0)
 	{
 		count++;
-		u=u-i;
-		if(u==0)
-		break;
+		u=u-i;	
 	}
-
-	cout<<count<<"COUNT"<<endl;
 	int  sum=count,t=count;
 	for(int i=1;i<=(2*count);i++)
 	{ int x;
-
 		x=i;
 		if(i>count)
 		{
 		x=count-(i%(count+1));
 		k=n-sum;
 		sum=sum+(--t);
-
 		}
 		for(int j=1;j<=x;j++)
 		{
 			if(i<=count)
 			{	cout<<k;
 				if(j!=x)
-					cout<<"*";
+				cout<<"*";
 				k++;
 			}
 		if(i>count)
 				cout<<k+j;
-				
 				if(j!=x)
-					cout<<"*";
+				cout<<"*";
 		}
 		cout<<endl;
-		
 	}
-			
 }
